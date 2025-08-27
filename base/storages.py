b076398eae582d9f4fc950995b7f23dc1e21d2bc
Bohdan Yagginov <bhders62@gmail.com>
2025-08-27 13:02:54 +0300

    disabled cache for media urls

--- base/storages.py
+++ base/storages.py
@@ -18,19 +18,19 @@ class WindowsCompatibleDropboxStorage(DropboxStorage):
         else:
             return safe_join(self.root_path, name).replace("\\", "/")
 
-    @staticmethod
-    def _get_cache_key(name: str) -> str:
-        return f"dropbox:media:{name}"
+    # @staticmethod
+    # def _get_cache_key(name: str) -> str:
+    #     return f"dropbox:media:{name}"
 
-    def url(self, name):
-        cache_key = self._get_cache_key(name)
-        if link := cache.get(cache_key):
-            return link
+    # def url(self, name):
+    #     cache_key = self._get_cache_key(name)
+    #     if link := cache.get(cache_key):
+    #         return link
 
-        try:
-            res = self.client.files_get_temporary_link(self._full_path(name))
-            link = res.link
-            cache.set(cache_key, link, self.CACHE_TTL)
-            return link
-        except ApiError:
-            return None
+    #     try:
+    #         res = self.client.files_get_temporary_link(self._full_path(name))
+    #         link = res.link
+    #         cache.set(cache_key, link, self.CACHE_TTL)
+    #         return link
+    #     except ApiError:
+    #         return None
